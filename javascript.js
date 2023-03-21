4ea4e819675bf6c6c72499cdce15e1e08e6152a2
zakio826 <124641420+zakio826@users.noreply.github.com>
2023-03-21 18:24:32 +0900

    提出ファイル (JavaScript)

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 /* 変数定義 */
 
-var memo = ["\n", "\n", "\n"]; // メモの全内容
-var title = ''; // メモタイトル（エディタ内要素）
-var text = ''; // メモ内容（エディタ内要素）
-var id = 0; // メモ番号（行頭番号-1）
+var memo = ["\n", "\n", "\n"]; // メモの全内容（ローカルストレージ格納）
+var title = ''; // メモタイトル（メモ編集画面内の要素）
+var text = ''; // メモ内容（メモ編集画面内の要素）
+var id = 0; // メモid（メモ一覧画面の行頭番号-1）
 
-// メモのリスト（html要素）
+// メモ一覧画面のHTML要素
 var list = [
     '<li><button name="memo_title" id="0" class="ol_common" onclick="load(0);">メモ 1</button></li><br>',
     '<li><button name="memo_title" id="1" class="ol_common" onclick="load(1);">メモ 2</button></li><br>',
@@ -13,18 +13,18 @@ var list = [
     '<li><input type="text" name="memo_title" id="new" value="新規メモ" class="ol_common"></li>'
 ];
 
-// リスト行のフォーマット（初期化用）
+// メモ一覧のHTML要素のフォーマット（初期化用）
 const list_format = [
     '<li><button name="memo_title" id="', '0', '" class="ol_common" onclick="load', '(', '0', ')', ';">', '', '</button></li><br>'
 ];
 
-// リスト行のフォーマット（list_edit.join('')を行うとリスト１行分になる）
-var list_edit = list_format;
-
-// リスト行の正規表現（最終行を除く）
+// メモ一覧のHTML要素の正規表現（最終行を除く）
 const list_regexp = new RegExp(`(${list_format[0]}).+(${list_format[2]}).+(${list_format[6]})`, 'g');
 
-// リスト行のフォーマット（最終行）
+// メモ一覧のHTML要素１行分（最終行を除く）
+var list_edit = list_format; // list_edit.join('')でリスト１行分になる
+
+// メモ一覧のHTML要素１行分（最終行）
 const list_end = '<li><input type="text" name="memo_title" id="new" value="新規メモ" class="ol_common"></li>';
 
 // 各種操作ボタン（html要素）
@@ -33,16 +33,15 @@ const btn_save = '<button id="save" class="" onclick="save();">保存</button>';
 const btn_del = '<button id="del" class="del" onclick="del();">削除</button>';
 
 
-
 /* 関数定義 */
 
 // メモアプリの初期化
 function reset() {
-    // メモ内容を初期化
+    // メモの全内容を初期化
     memo = ["\n", "\n", "\n"];
     localStorage.setItem('memo', memo);
 
-    // メモのリストを初期化
+    // メモ一覧画面を初期化
     list = [
         '<li><button name="memo_title" id="0" class="ol_common" onclick="load(0);">メモ 1</button></li><br>',
         '<li><button name="memo_title" id="1" class="ol_common" onclick="load(1);">メモ 2</button></li><br>',
@@ -55,7 +54,7 @@ function reset() {
     console.clear();
 }
 
-// データ取得
+// データの取得
 function open() {
     // ローカルストレージをチェック（データが無い場合はメモアプリの初期化を実行）
     if (!localStorage.getItem('memo') || !localStorage.getItem('list')) {
@@ -63,7 +62,7 @@ function open() {
         reset();
     }
     
-    // ローカルストレージの各データの末尾に改行を一つだけ付与
+    // ローカルストレージから各メモ内容の末尾に改行を一つだけ付与
     memo = localStorage.getItem('memo').split('\n,');
     memo = memo.map(value => value + '\n');
     memo = memo.map(value => value.replace(/\n+$/, '\n'));
@@ -94,7 +93,7 @@ function load(id) {
     console.log(`id = ${id}`);
     
     document.getElementById('title').value = title;
-    document.getElementById('text').value = text.replace(/\n$/, ''); // 末尾の改行を削除
+    document.getElementById('text').value = text.replace(/\n$/, ''); // メモ内容末尾の改行を削除
     document.getElementById('id').value = String(id);
 }
 
@@ -110,7 +109,7 @@ function save() {
     console.log(`text_in = ${text}`);
     console.log(`id_in = ${id}`);
 
-    // リストのメモタイトルを変更
+    // メモ一覧画面のタイトルを変更
     document.getElementById(String(id)).innerHTML = title;
     list = document.getElementById('list').innerHTML.split('\n');
     localStorage.setItem('list', list.join('\n,'));
@@ -136,15 +135,13 @@ function create() {
     memo.push("\n");
     localStorage.setItem('memo', memo);
 
-    // リスト行のフォーマットを編集
+    // 新規メモタイトルを一覧画面のHTML要素に追加
+    list_edit = list_format; // HTML要素をフォーマット
     list_edit[1] = list_edit[4] = String(id);
     list_edit[7] = title;
-
-    // リスト行のフォーマットを末尾に追加
     list.pop();
     list.push(list_edit.join(''), list_end);
 
-    list_edit = list_format; // リストのフォーマットを初期化
     document.getElementById('list').innerHTML = list.join('\n');
     localStorage.setItem('list', list.join('\n,'));
     
@@ -161,16 +158,16 @@ function del() {
     id = Number(document.getElementById('id').value);
     
     console.log(`title_del = ${title}`);
-    console.log(`id_del = ${id}`);
+    console.log(`id_del = ${id+1}`);
 
     // ローカルストレージから指定のメモ内容を削除
     memo.splice(id, 1);
     console.log(`memo =\n${memo}`);
     localStorage.setItem('memo', memo);
 
-    list.splice(id, 1); // リスト行から指定のメモタイトルを削除
+    list.splice(id, 1); // メモ一覧画面から指定のタイトルを削除
 
-    // リスト行のidを修復
+    // メモidを修復
     for (id; id < memo.length; id++) {
         title = list[id];
         list[id] = title.replace(list_regexp, `$1${String(id)}$2(${String(id)})$3`);
